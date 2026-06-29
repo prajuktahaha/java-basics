7c8a634be8f1cf07f3a3f721a9a9701ef4867475
Prajukta <prajuktaha@gmail.com>
2026-06-29 23:59:20 +0530

    even nno

---
+++ evennumbers1to100.java
@@ -0,0 +1,11 @@
+public class evenonetohundred {
+    public static void main(String[] args) {
+        int i;
+        for(i = 1 ; i<=100 ; i++){
+            if(i%2==0){
+                System.out.println(i);
+            }
+            
+        }
+    }
+}
